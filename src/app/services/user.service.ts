5e55217742e06b3a1fc63c8b83edadb9379ffc24
JFritzel <jfritzel@ROAM156.local>
2017-12-12 15:33:07 -0600

    Added base url for api to user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
+import {environment} from '../../environments/environment';
 import {User} from '../models/user';
 import {MessageService} from './message.service';
 import {PaymentService} from './payment.service';
@@ -8,31 +8,33 @@ import {MaintenanceService} from './maintenance.service';
 
 @Injectable()
 export class UserService {
+  private baseURL = environment.api.baseUrl;
+
   constructor(private http: HttpClient,
               private messageService: MessageService) {
   }
 
   getAll() {
-    return this.http.get<User[]>('/api/users');
+    return this.http.get<User[]>(this.baseURL + '/api/users');
   }
 
   getById(id: number) {
-    return this.http.get('/api/users/' + id)
+    return this.http.get(this.baseURL + '/api/users/' + id)
       .map((response: Response) => response.json());
   }
 
   create(user: User) {
-    return this.http.post('/api/users', user)
+    return this.http.post(this.baseURL + '/api/users', user)
       .map((response: Response) => response.json());
   }
 
   update(user: User) {
-    return this.http.put('/api/users/' + user.id, user)
+    return this.http.put(this.baseURL + '/api/users/' + user.id, user)
       .map((response: Response) => response.json());
   }
 
   delete(id: number) {
-    return this.http.delete('/api/users/' + id)
+    return this.http.delete(this.baseURL + '/api/users/' + id)
       .map((response: Response) => response.json());
   }
 }
