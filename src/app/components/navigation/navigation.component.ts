ae76aa651189ff011c203ae6de5392eb1da60fe2
Scott Near <snear@betternoi.com>
2017-12-20 17:45:12 -0600

    implemented new layout

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+    public isCollapsed = true;
+
+    ngOnInit() {
+
+    }
+
+}
