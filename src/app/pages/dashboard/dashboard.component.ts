572dbe3a9de7bc6e3ddb064e36531060e975cd6b
Scott Near <snear@betternoi.com>
2018-01-23 14:50:13 -0600

    finish switch no consolelogs

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -40,54 +40,31 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.session.getObservable('currentUser')
       .subscribe((user: User) => this.currentUser = user);
-      console.log('prev site from session');
-      console.log(localStorage.getItem('currentSiteId'));
       this.currentSiteId = Number (localStorage.getItem('currentSiteId'));
-      console.log('prev set current site');
-      console.log(this.currentSiteId);
       this.userService.getCurrentUserInfo().subscribe(
             data => {
                 this.userSites = data['rentalSites'];
-                console.log('userSites ARRAY');
-                console.log(this.userSites);
                 if (this.userSites.length > 1) {
                   this.multiSite = true;
                 }
-                console.log('mySite?:');
-                console.log(this.mySite);
                 if (this.mySite) {
                    for (const userSite of this.userSites) {
                      if (userSite.id === this.mySite) {
                        this.currentSite = userSite;
                      }
                    }
-                  console.log('new site from param');
                   localStorage.setItem('currentSiteId', this.currentSite.id);
                 } else if (this.currentSiteId) {
-                  console.log('current site exists');
-                  console.log(this.userSites);
                   for (const userSite of this.userSites) {
                      if (userSite.id === this.currentSiteId) {
                        this.currentSite = userSite;
                      }
                    }
-                  console.log('before set localstorage');
-                  console.log(this.currentSite);
                   localStorage.setItem('currentSiteId', this.currentSite.id);
-                  console.log('after set local storage');
-                  console.log(this.currentSite);
                 } else {
-                  console.log('no current site or param');
                   this.currentSite = this.userSites[this.defaultSite];
                   localStorage.setItem('currentSiteId', this.currentSite.id);
-                  console.log(localStorage);
                 }
-                console.log('end currentSite');
-                console.log(this.currentSite);
-                console.log('end currentSiteId var');
-                console.log(this.currentSiteId);
-                console.log('end currentSiteId localstorage');
-                console.log(localStorage.getItem('currentSiteId'));
             },
             error => {
                 this.alertService.error('Unable to retrieve site');
