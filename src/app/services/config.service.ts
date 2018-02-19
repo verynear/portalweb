5d71dc1626fdb3153b9a8f7614b8e43233d7a527
JFritzel <jfritzel@ROAM156.local>
2018-02-19 15:30:49 -0600

    Try #3

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -11,14 +11,15 @@ export class ConfigService {
     // localhost
     const parts = location.hostname.split('.'),
       site = parts.shift(),
-      host = parts.join('.') || 'devdemo.betterleasing.com';
+      host = parts.join('.') || 'devdemo.betterleasing.com',
+      local = parts[0]; // for localhost.
 
     this.data = {
       api: {
         baseURL: `${location.protocol}//api.${host}` + '/leasenet'
       },
       customer: {
-        subdomain: site,
+        subdomain: site || local,
         host
       },
       environments: ['devdemo', 'stage', 'api', 'localhost']
@@ -26,6 +27,25 @@ export class ConfigService {
   }
 
   get(): any {
+    const parts = location.hostname.split('.'),
+      site = parts.shift(),
+      host = parts.join('.') || 'devdemo.betterleasing.com',
+      local = parts[0]; // for localhost.
+
+    console.log('Parts');
+    console.log(parts);
+
+    console.log('Site');
+    console.log(site);
+
+    console.log('Host');
+    console.log(host);
+
+    console.log('Local');
+    console.log(local);
+
+
+    console.log('Returning from Config Service');
     return this.data;
   }
 
