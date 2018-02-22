0493840ce8d2b137795ab2d57a9d076b6b3d4afa
JFritzel <jfritzel@ROAM156.local>
2018-02-22 16:03:57 -0600

    Very basic report list, just for testing

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SiteService } from '../../services/site.service';
+import { MessageService } from '../../services/message.service';
+
+import { User } from '../../models/user';
+import { Site } from '../../models/site';
+import { Building } from '../../models/building';
+import { Message } from '../../models/message';
 
 @Component({
   selector: 'app-report',
@@ -7,10 +14,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  public currentSite: Site;
+  public sites: Site[];
+  public buildings: Building[];
+  public messages: Message[];
+
+  constructor(private siteService: SiteService, private messageService: MessageService) {
+
+  }
 
   ngOnInit() {
+    this.siteService.getCurrentSite().subscribe(site => {
+      this.currentSite = site;
+      this.getRentalBuildings(site.id);
+
+      this.getMessages();
+    });
+  }
+
+  getRentalSites() {
+    this.siteService.getRentalSites().subscribe(sites => {
+      this.sites = sites;
+    });
+  }
+
+  getRentalBuildings(currSiteId: number) {
+    this.siteService.getBuildings(currSiteId).subscribe(buildings => {
+      this.buildings = buildings;
+    });
+  }
+
+  getMessages() {
+    const page = 1;
+    const size = 10;
+
+    this.messageService.getSent(page, size).subscribe(messages => {
+      console.log('Messages...');
+      this.messages = messages['messages'];
+      console.log(this.messages);
+    });
 
   }
 
 }
+
+
