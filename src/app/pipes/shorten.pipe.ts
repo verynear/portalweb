2965b954312e763616cde1966dd2bce5cbcc593c
JFritzel <jfritzel@ROAM156.local>
2018-02-22 11:40:14 -0600

    Adding Shorten Pipe: see shorten.pipe.ts for usage

--- src/app/pipes/shorten.pipe.ts
+++ src/app/pipes/shorten.pipe.ts
@@ -1,12 +1,39 @@
+/*
+  This library is part of danrevah / ngx-pipes
+  See on Github for documentation.
+
+  Shortening a string by length and providing a custom string to denote an omission
+  Usage: string | shorten: length: [suffix|optional]: [wordBreak boolean|optional]
+
+  <p>{{'Hey foo bar' | shorten: 3: '...'}}</p> <!-- Output: "Hey..." -->
+*/
+
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'shorten'
-})
+@Pipe({name: 'shorten'})
 export class ShortenPipe implements PipeTransform {
+  transform(input: string, length?: number, suffix?: string, wordBreak?: boolean): string;
+  transform(input: any, length?: number, suffix?: string, wordBreak?: boolean): any;
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(text: any, length: number = 0, suffix: string = '', wordBreak: boolean = true): string {
+    if (!isString(text)) {
+      return text;
+    }
+
+    if (text.length > length) {
+      if (wordBreak) {
+        return text.slice(0, length) + suffix;
+      }
+
+      // tslint:disable-next-line:no-bitwise
+      if (!!~text.indexOf(' ', length)) {
+        return text.slice(0, text.indexOf(' ', length)) + suffix;
+      }
+    }
+    return text;
   }
+}
 
+export function isString(value: any) {
+  return typeof value === 'string';
 }
