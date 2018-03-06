4544c6d32790d249be549a5cedb7ec17edeb84ad
verynear <scott.a.near@gmail.com>
2018-03-06 17:40:35 -0600

    import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { MockBackend } from '@angular/http/testing';
 import { BaseRequestOptions } from '@angular/http';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { InputTextModule, ButtonModule, DataTableModule, DialogModule, SharedModule, CheckboxModule } from 'primeng/primeng';
+import { InputTextModule, ButtonModule, DialogModule, SharedModule, CheckboxModule } from 'primeng/primeng';
 import { TableModule } from 'primeng/components/table/table';
 
 import { AppComponent } from './app.component';
@@ -89,6 +89,7 @@ import { BuildingReportComponent } from './pages/report/building-report/building
     ButtonModule,
     DialogModule,
     SharedModule,
+    TableModule,
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
