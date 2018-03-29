29011c727dd69bfc2e2655cbeb07ef90a143f3e1
Scott Near <snear@betternoi.com>
2018-03-29 18:15:47 -0500

    manual applicant add

--- src/app/pages/leasing/leasing.component.ts
+++ src/app/pages/leasing/leasing.component.ts
@@ -4,6 +4,7 @@ import { ApplicantService } from '../../services/applicant.service';
 import { AlertService } from '../../services/alert.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GuestcardComponent } from '../../components/guestcard/guestcard.component';
+import { ApproveformComponent } from '../../components/approveform/approveform.component';
 
 @Component({
   selector: 'app-leasing',
@@ -14,32 +15,44 @@ export class LeasingComponent implements OnInit {
 
   applicant: Applicant = new Applicant();
   selectedApplicant: Applicant;
-  newApplicant: boolean;
   applicants: Applicant[];
+  loading: boolean;
 
   constructor(private applicantService: ApplicantService, private alertService: AlertService, private modalService: NgbModal) { }
 
   ngOnInit() {
+     this.loading = true;
      this.getApplicants();
   }
 
   getApplicants() {
+      this.loading = true;
       this.applicantService.get().subscribe(
       data => {
+        this.loading = false;
         console.log('Applicants Loaded');
         this.applicants = data;
         console.log(data[0].lastname);
       },
       error => {
         console.log('Error');
+        this.loading = false;
       });
     }
+
+  refresh() {
+    this.getApplicants();
+  }
 
   openCard(applicant) {
     const modalRef = this.modalService.open(GuestcardComponent, { windowClass: 'dark-modal' });
     modalRef.componentInstance.name = applicant.firstname + ' ' + applicant.lastname;
     modalRef.componentInstance.phone = applicant.phone;
     modalRef.componentInstance.moveinDate = applicant.moveinDate;
+  }
+
+  newApplicant() {
+    const modalRef = this.modalService.open(ApproveformComponent);
   }
 
   // For sort event./
